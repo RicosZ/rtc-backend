4176fb7bb5c65cd5adc2fd955523666ac44cc7c0
Kitticahi <57173057+RicosZ@users.noreply.github.com>
2023-02-08 14:44:49 +0700

    socket rtc

--- controllers/meeting.controller.js
+++ controllers/meeting.controller.js
@@ -1,7 +1,7 @@
 const meetingServices = require('../services/meeting.service');
 
 exports.startMeeting = (req, res, next) => {
-    console.log('request');
+    // console.log('request');
     const { hostId, hostName } = req.body;
 
     var model = {
@@ -38,6 +38,16 @@ exports.getAllMeetingUsers = (req, res, next) => {
     const { meetingId } = req.query;
     meetingServices.getAllMeetingUsers(meetingId, (error, results) => {
         if (error) return next(error);
+        return res.status(200).send({
+            message: 'Success',
+            data: results,
+        });
+    })
+}
+exports.updateMeetingUser = (req,res,next)=>{
+    const {userId} = req.body;
+    meetingServices.updateMeetingUser(userId,(error,results) =>{
+        if(error) return next(error);
         return res.status(200).send({
             message: 'Success',
             data: results,
--- index.js
+++ index.js
@@ -1,27 +1,64 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
-const { MONGO_DB_CONFIG } = require('./config/app.config');
-const http = require('http');
-const server = http.createServer(app);
-const {initMeetingServer} = require('./meeting-server');
+// const { Server } = require('socket.io');
+// const mongoose = require('mongoose');
+// const { MONGO_DB_CONFIG } = require('./config/app.config');
 
-initMeetingServer(server);
+// const {initMeetingServer} = require('./meeting-server');
 
+// initMeetingServer(server);
 
-mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_DB_CONFIG.DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('Database Connected');
-}, (error) => {
-    console.log(error.message);
-});
+async function server() {
+    const http = require('http').createServer(app);
+    const io = require('socket.io')(http,{ transports: ['websocket'] });
+    const roomName = 'room1';
+    io.on('connection', (socket)=>{
+        socket.on('join',()=>{
+            socket.join(roomName);
+            socket.to(roomName).emit('joined');
+            // socket.emit('joined');
+            console.log(io.sockets.adapter.rooms[roomName].length);
+            console.log('join');
+        });
+        
+        socket.on('offer',(offer)=>{
+            // console.log(offer);
+            socket.to(roomName).emit('offer',offer);
+            // socket.emit('offer',offer);
+            console.log('offer');
+        });
+        socket.on('answer',(answer)=>{
+            socket.to(roomName).emit('answer',answer);
+            // socket.emit('answer',answer);
+            console.log('answer');
+        });
+        socket.on('candidiate',(candidiate)=>{
+            socket.to(roomName).emit('candidiate',candidiate);
+            // socket.emit('ice',ice);
+            console.log('candidiate');
+        });
+    });
+    const port = process.env.port || 4444;
+    http.listen(port, function () {
+        console.log(`API Ready!!! on ${port}`);
+    });
+}
 
-app.use(express.json());
-app.use('/api/v1', require('./routes/app.routes'));
+server();
 
-server.listen(process.env.port || 4444, function () {
-    console.log('API Ready!!!');
-});
+// mongoose.Promise = global.Promise;
+// mongoose.connect(MONGO_DB_CONFIG.DB, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// }).then(() => {
+//     console.log('Database Connected');
+// }, (error) => {
+//     console.log(error.message);
+// });
+
+// app.use(express.json());
+// app.use('/api/v1', require('./routes/app.routes'));
+
+// http.listen(process.env.port || 4444, function () {
+//     console.log('API Ready!!!');
+// });
